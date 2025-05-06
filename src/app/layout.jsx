0649c834185f88400514c965b74f65b2812456fe
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-05-06 16:57:04 +0330

    Reset files for developing

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+import "./styles/globals.css";
+
+export const metadata = {
+  title: "Shop App Project",
+  description: "An app for shopping anything",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-center text-xl mt-4">Shop App Project</div>;
+}
