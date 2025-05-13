fb0f027ecb380c74d81ed13210097f9ebfed7b28
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-05-13 16:20:11 +0330

    Add absolute imports to project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="mt-4 text-center text-3xl font-bold">Shop App Project</h1>
+    <h1 className="mt-4 text-center text-3xl font-bold text-secondary-800">
+      Shop App Project
+    </h1>
   );
 }
