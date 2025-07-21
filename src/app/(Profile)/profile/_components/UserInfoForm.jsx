31c7a17575f211a1ffddd1f492dda2376700dac4
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-07-21 10:51:13 +0330

    Develop UserInfoForm component and update UserInfoPage

---
+++ src/app/(Profile)/profile/_components/UserInfoForm.jsx
@@ -0,0 +1,88 @@
+"use client";
+
+import RHFTextField from "@/ui/RHFTextField";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    phoneNumber: yup
+      .string()
+      .required("شماره موبایل الزامی است")
+      .matches(/^09\d{9}$/, "شماره موبایل معتبر نیست"),
+    email: yup.string().required("ایمیل الزامی است").email("ایمیل معتبر نیست"),
+    userName: yup
+      .string()
+      .required("نام کاربری الزامی است")
+      .min(5, "نام کاربری باید حداقل ۵ کاراکتر باشد"),
+  })
+  .required();
+
+function UserInfoForm({ user }) {
+  const { phoneNumber, email, name: userName, biography } = user;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { phoneNumber, email, userName, biography },
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="formLayout grid w-full grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2"
+    >
+      <RHFTextField
+        label="شماره موبایل"
+        register={register}
+        errors={errors}
+        name="phoneNumber"
+        dir="ltr"
+        type="number"
+        isRequired
+      />
+
+      <RHFTextField
+        label="ایمیل"
+        register={register}
+        errors={errors}
+        name="email"
+        dir="ltr"
+        type="email"
+        isRequired
+      />
+
+      <RHFTextField
+        label="نام کاربری"
+        register={register}
+        errors={errors}
+        name="userName"
+        isRequired
+      />
+
+      <RHFTextField
+        label="بیوگرافی"
+        register={register}
+        errors={errors}
+        name="biography"
+      />
+
+      <button
+        type="submit"
+        className="btn btn--primary mt-2 self-center justify-self-start px-6 sm:px-8 lg:px-14"
+      >
+        تایید اطلاعات
+      </button>
+    </form>
+  );
+}
+export default UserInfoForm;
--- src/app/(Profile)/profile/me/page.jsx
+++ src/app/(Profile)/profile/me/page.jsx
@@ -1,4 +1,37 @@
+"use client";
+
+import useUser from "@/hooks/useUser";
+import { PulseLoader } from "react-spinners";
+import UserInfoForm from "../_components/UserInfoForm";
+
 function UserInfoPage() {
-  return <div>User Info Page</div>;
+  const { isLoading, user } = useUser();
+
+  return (
+    <div className="flex flex-col items-start gap-6">
+      <div className="flex flex-col items-start gap-2">
+        <h2 className="font-bold text-black sm:text-lg lg:text-xl">
+          فرم اطلاعات کاربری
+        </h2>
+
+        <p className="text-sm text-secondary-800 lg:text-base">
+          میتوانید اطلاعات کاربری خود را در فرم زیر تغییر دهید.
+        </p>
+      </div>
+
+      {isLoading && (
+        <div className="flex w-full items-center justify-center">
+          <span className="ml-2 text-primary-900">در حال بارگذاری</span>
+          <PulseLoader
+            color="rgb(var(--color-primary-900))"
+            size={12}
+            margin={3}
+          />
+        </div>
+      )}
+
+      {!isLoading && user && <UserInfoForm user={user} />}
+    </div>
+  );
 }
 export default UserInfoPage;
