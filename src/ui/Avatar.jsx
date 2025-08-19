fcfeedfd3303f3ea28bc9a981da80998a7894817
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-08-19 11:44:25 +0330

    Fix the Avatar component bug

--- src/ui/Avatar.jsx
+++ src/ui/Avatar.jsx
@@ -2,13 +2,21 @@ import Image from "next/image";
 
 function Avatar({ src, size = 24 }) {
   return (
-    <Image
-      src={src || "/images/avatar.png"}
-      width={size}
-      height={size}
-      className="rounded-full ring-1 ring-secondary-300"
-      alt="user avatar"
-    />
+    <div
+      style={{
+        width: size,
+        height: size,
+      }}
+      className={`relative overflow-hidden rounded-full ring-1 ring-secondary-300`}
+    >
+      <Image
+        src={src || "/images/avatar.png"}
+        fill
+        priority
+        className="object-cover"
+        alt="user avatar"
+      />
+    </div>
   );
 }
 export default Avatar;
