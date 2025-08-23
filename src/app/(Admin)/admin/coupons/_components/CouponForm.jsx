e579e375e38b464f5341696d47a8519fda976982
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-08-23 17:27:53 +0330

    Develop CouponForm component UI

---
+++ src/app/(Admin)/admin/coupons/_components/CouponForm.jsx
@@ -0,0 +1,184 @@
+"use client";
+
+import RHFTextField from "@/ui/RHFTextField";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import SvgLoaderComponent from "@/ui/SvgLoaderComponent";
+import toast from "react-hot-toast";
+import RHFSelect from "@/ui/RHFSelect";
+import useGetCategories from "@/hooks/useGetCategories";
+import RHFDatePicker from "@/ui/RHFDatePicker";
+
+const schema = yup
+  .object({
+    code: yup
+      .string()
+      .required("کد تخفیف الزامی است.")
+      .min(5, "کد تخفیف باید حداقل ۵ کاراکتر باشد.")
+      .max(30, "کد تخفیف نباید بیشتر از ۳۰ کاراکتر باشد."),
+
+    amount: yup
+      .string()
+      .required("مقدار الزامی است.")
+      .matches(/^\d+$/, "مقدار باید عدد باشد.")
+      .test("not-zero", "مقدار نمی‌تواند ۰ باشد.", (value) => value !== "0")
+      .when("type", (type, schema) => {
+        if (type[0]?.value === "percent") {
+          return schema.test(
+            "max-100",
+            "برای تخفیف درصدی مقدار نمی‌تواند بیشتر از ۱۰۰ باشد.",
+            (value) => {
+              if (!value) return false;
+              return Number(value) <= 100;
+            },
+          );
+        }
+        return schema;
+      }),
+
+    usageLimit: yup
+      .string()
+      .required("ظرفیت الزامی است.")
+      .matches(/^\d+$/, "ظرفیت باید عدد باشد.")
+      .test("not-zero", "ظرفیت نمی‌تواند ۰ باشد.", (value) => value !== "0"),
+
+    type: yup
+      .object()
+      .typeError("نوع تخفیف الزامی است.")
+      .required("نوع تخفیف الزامی است."),
+
+    productIds: yup
+      .array()
+      .of(yup.object().typeError("محصولات الزامی است."))
+      .required("محصولات الزامی است.")
+      .min(1, "حداقل یک محصول باید انتخاب شود."),
+
+    expireDate: yup.date().required("تاریخ انقضا الزامی است."),
+  })
+  .required();
+
+function CouponForm({ initialData = {}, isUpdating = false }) {
+  const { isLoading, categories } = useGetCategories();
+
+  const isAdding = false;
+  const isEditing = false;
+
+  const initialValues = isUpdating
+    ? {
+        code: initialData.code,
+        amount: initialData.amount,
+        type: {
+          value: initialData.type,
+          label: initialData.type === "percent" ? "درصدی" : "قیمت‌ ثابت",
+        },
+        productIds: initialData.productIds.map((product) => {
+          return { value: product._id, label: product.title };
+        }),
+        expireDate: initialData.expireDate,
+      }
+    : {};
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors, isDirty },
+  } = useForm({
+    defaultValues: initialValues,
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = (values) => {
+    if (isUpdating) {
+      if (!isDirty) return toast.error("لطفا حداقل یک فیلد را تغییر دهید.");
+      console.log(values);
+
+      //  Handle edit category
+    } else {
+      //  Handle add category
+      console.log(values);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="formLayout grid w-full grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2 xl:grid-cols-3"
+    >
+      <RHFTextField
+        label="کد تخفیف"
+        register={register}
+        errors={errors}
+        name="code"
+        dir="ltr"
+        isRequired
+      />
+
+      <RHFTextField
+        label="مقدار"
+        register={register}
+        errors={errors}
+        name="amount"
+        dir="ltr"
+        isRequired
+      />
+
+      <RHFTextField
+        label="ظرفیت"
+        register={register}
+        errors={errors}
+        name="usageLimit"
+        dir="ltr"
+        isRequired
+      />
+
+      <RHFSelect
+        label="نوع تخفیف"
+        control={control}
+        name="type"
+        placeholder="یک گزینه را انتخاب کنید."
+        options={[
+          { value: "percent", label: "درصدی" },
+          { value: "fixedProduct", label: "قیمت‌ ثابت" },
+        ]}
+        isRequired
+      />
+
+      <RHFSelect
+        label="محصولات"
+        control={control}
+        name="productIds"
+        placeholder="محصولات را انتخاب کنید."
+        options={
+          !isLoading
+            ? categories.map((category) => {
+                return { value: category._id, label: category.title };
+              })
+            : []
+        }
+        isRequired
+        isMulti
+      />
+
+      <RHFDatePicker
+        label="تاریخ انقضا"
+        control={control}
+        name="expireDate"
+        placeholder="تاریخ انقضا را انتخاب کنید."
+        isRequired
+      />
+
+      <button
+        type="submit"
+        disabled={isAdding || isEditing}
+        className={`btn ${isAdding || isEditing ? "btn--outline" : "btn--primary"} mt-2 self-center justify-self-start px-6 sm:col-span-2 sm:px-8 lg:px-14 xl:col-span-3`}
+      >
+        {isAdding || isEditing ? <SvgLoaderComponent /> : "ثبت دسته‌بندی‌"}
+      </button>
+    </form>
+  );
+}
+export default CouponForm;
---
+++ src/app/(Admin)/admin/coupons/create/page.jsx
@@ -0,0 +1,22 @@
+import BackButton from "@/ui/BackButton";
+import { FaChevronRight } from "react-icons/fa6";
+import CouponForm from "../_components/CouponForm";
+
+function CreateCouponPage() {
+  return (
+    <div>
+      <div className="mb-6 flex items-center gap-1 md:gap-2">
+        <BackButton>
+          <FaChevronRight />
+        </BackButton>
+
+        <h2 className="text-lg font-bold">ایجاد کد تخفیف جدید</h2>
+      </div>
+
+      <div>
+        <CouponForm />
+      </div>
+    </div>
+  );
+}
+export default CreateCouponPage;
