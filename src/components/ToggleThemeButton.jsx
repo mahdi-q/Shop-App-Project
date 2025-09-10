5db52ffb6ec91406c400ed4710dbfd2fe7efcb33
mahdi-q <ghasemi84mahdi@gmail.com>
2025-09-10 14:13:47 +0330

    Fix the hydration error

--- src/components/ToggleThemeButton.jsx
+++ src/components/ToggleThemeButton.jsx
@@ -1,8 +1,19 @@
+"use client";
+
 import { useDarkMode } from "@/contexts/DarkModeContext";
+import { useEffect, useState } from "react";
 import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
 
 function ToggleThemeButton({ className = "" }) {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    //  Now we are sure that the component is mounted
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <button onClick={toggleDarkMode} className={className}>
